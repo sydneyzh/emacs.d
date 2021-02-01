394c45bcf06e91d8c046a35002feb2cccb28eeaf
aws <sydneyzh@gmail.com>
2021-02-01 05:17:00 +0000

    Added tsx mode config. Various fix on init-local

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,48 +5,56 @@
 ;;; use-package
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
+
+;;; auto-complete
+(use-package auto-complete
+  :ensure t)
 
 ;;; evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-(require 'evil)
-(evil-mode 1)
-(cl-loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
-                              (nrepl-mode . insert)
-                              (pylookup-mode . emacs)
-                              (comint-mode . normal)
-                              (shell-mode . insert)
-                              (git-commit-mode . insert)
-                              (git-rebase-mode . emacs)
-                              (term-mode . emacs)
-                              (help-mode . emacs)
-                              (helm-grep-mode . emacs)
-                              (grep-mode . emacs)
-                              (bc-menu-mode . emacs)
-                              (magit-mode . emacs)
-                              (magit-branch-manager-mode . emacs)
-                              (rdictcc-buffer-mode . emacs)
-                              (dired-mode . emacs)
-                              (wdired-mode . normal)
-                              (xref--xref-buffer-mode . emacs))
-      do (evil-set-initial-state mode state))
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  (cl-loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
+                                   (nrepl-mode . insert)
+                                   (pylookup-mode . emacs)
+                                   (comint-mode . normal)
+                                   (shell-mode . insert)
+                                   (git-commit-mode . insert)
+                                   (git-rebase-mode . emacs)
+                                   (term-mode . emacs)
+                                   (help-mode . emacs)
+                                   (helm-grep-mode . emacs)
+                                   (grep-mode . emacs)
+                                   (bc-menu-mode . emacs)
+                                   (magit-mode . emacs)
+                                   (magit-branch-manager-mode . emacs)
+                                   (rdictcc-buffer-mode . emacs)
+                                   (dired-mode . emacs)
+                                   (wdired-mode . normal)
+                                   (xref--xref-buffer-mode . emacs))
+           do (evil-set-initial-state mode state))
+  )
 
 ;; undo-tree
 (use-package undo-tree
-  :diminish undo-tree-mode)
-(global-undo-tree-mode)
-(define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
-(define-key evil-normal-state-map (kbd "u") 'undo-tree-undo)
+  :ensure t
+  :diminish undo-tree-mode
+  :after evil
+  :config
+  (global-undo-tree-mode)
+  (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
+  (define-key evil-normal-state-map (kbd "u") 'undo-tree-undo)
+  )
 
 ;;; popwin
 (use-package popwin
+  :ensure t
   :diminish popwin-mode
   :config
   ;; config "popwin:special-display-config"
   (push '("*Anaconda*" :dedicated t :position right :stick t :noselect t :width 0.3) popwin:special-display-config)
-  (push '("*Autocomplete Error*" :dedicated t :position bottom :stick t :noselect t :height 0.2) popwin:special-display-config))
+  (push '("*Autocomplete Error*" :dedicated t :position bottom :stick t :noselect t :height 0.2) popwin:special-display-config)
   (push '("*Autocomplete Output*" :dedicated t :position bottom :stick t :noselect t :height 0.2) popwin:special-display-config)
   (push '("*Backtrace*" :dedicated t :position right :stick t :noselect t :width 0.5) popwin:special-display-config)
   (push '("*Flycheck error messages*" :dedicated t :position right :stick t :noselect t :height 0.1) popwin:special-display-config)
@@ -56,27 +64,32 @@
   (push '("*Shell Command Output*" :dedicated t :position bottom :stick t :noselect t :height 0.1) popwin:special-display-config)
   (push '("*Warnings*" :dedicated t :position bottom :stick t :noselect t :height 0.2) popwin:special-display-config)
   (push '("*xref*" :dedicated t :position bottom :stick t :noselect t :height 0.2) popwin:special-display-config)
-(popwin-mode 1)
+  (popwin-mode 1)
+  )
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;;; jinja2-mode
 (use-package jinja2-mode
+  :ensure t
+  :mode ("\\.html?\\'" . jinja2-mode)
   :config
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . jinja2-mode))
   (superword-mode)
   )
 
 ;;; neotree
 (use-package neotree
-  :config
+  :ensure t
+  :init
   (global-set-key [f8] 'neotree-toggle))
 
 ;;; winum
 (use-package winum
-  :config
-  (winum-set-keymap-prefix (kbd "C-c")))
-(winum-mode)
+  :ensure t
+  :init
+  (winum-set-keymap-prefix (kbd "C-c"))
+  (winum-mode)
+  )
 
 ;;; customize switch-window-shortcut-style
 (when (maybe-require-package 'switch-window)
@@ -84,23 +97,40 @@
 
 ;;; yasnippet
 (use-package yasnippet
+  :ensure t
   :config
   (when (boundp '*snippet-dir*)
-    (setq yas-snippet-dirs '(*snippet-dir*))))
-(yas-global-mode 1)
+    (setq yas-snippet-dirs '(*snippet-dir*)))
+  (yas-global-mode 1))
 
 ;;; python
 (use-package pyenv-mode
+  :ensure t
   :hook python-mode)
 (use-package pyenv-mode-auto
-             :after pyenv-mode)
+  :ensure t
+  :after pyenv-mode)
 (when (maybe-require-package 'flycheck)
   (setq flycheck-python-pycompile-executable "python")) ;; override default "python3"
 
+;;; web-mode
+(use-package web-mode
+  :ensure t
+  :after flycheck
+  :mode "\\.tsx\\'"
+  :init
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (tide-setup))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
+
 ;;; tide
 (use-package tide
-  :after (typescript-mode company)
-  :config
+  :ensure t
+  :after typescript-mode
+  :init
   (defun setup-tide-mode ()
     (interactive)
     (tide-setup)
@@ -110,19 +140,6 @@
   (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'setup-tide-mode)
   )
-
-;; open tsx in web mode
-(use-package web-mode
-  :ensure t
-  :after (tide flycheck)
-  :config
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (when (string-equal "tsx" (file-name-extension buffer-file-name))
-                (setup-tide-mode))))
-  ;; enable typescript-tslint checker
-  (flycheck-add-mode 'typescript-tslint 'web-mode))
 
 ;;; org mode
 (with-eval-after-load 'org
